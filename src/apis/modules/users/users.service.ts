ab59df88dac7fc7cc47d5bfd263d38f1dfd602d4
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2023-07-04 15:25:06 +0700

    :money_with_wings: add docker compose && update service

--- src/apis/modules/users/users.service.ts
+++ src/apis/modules/users/users.service.ts
@@ -1,29 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { User } from '@prisma/client';
 import { CreateUserInput, UpdateUserInput } from './dto/user.dto';
+import { UserModel } from 'src/models/user.model';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async getUser(id: number): Promise<User> {
+  async getUser(id: number): Promise<UserModel> {
     return this.prisma.user.findUnique({ where: { id } });
   }
 
-  async getUsers(): Promise<User[]> {
+  async getUsers(): Promise<UserModel[]> {
     return this.prisma.user.findMany();
   }
 
-  async createUser(data: CreateUserInput): Promise<User> {
+  async createUser(data: CreateUserInput): Promise<UserModel> {
     return this.prisma.user.create({ data });
   }
 
-  async updateUser(id: number, data: UpdateUserInput): Promise<User> {
+  async updateUser(id: number, data: UpdateUserInput): Promise<UserModel> {
     return this.prisma.user.update({ where: { id }, data });
   }
 
-  async deleteUser(id: number): Promise<User> {
+  async deleteUser(id: number): Promise<UserModel> {
     return this.prisma.user.delete({ where: { id } });
   }
 }
